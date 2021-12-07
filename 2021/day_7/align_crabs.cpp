9fd0dbcde2147ce2416754a48d9c3a754a3a8914
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2021-12-07 11:48:19 +0100

    Day 7 solution. Binary search

--- 2021/day_7/align_crabs.cpp
+++ 2021/day_7/align_crabs.cpp
@@ -8,13 +8,23 @@
 using CrabsCount = std::unordered_map<int, int>;
 using AlignCost = unsigned long long;
 
-AlignCost get_const_crab_move_cost(int from, int to) {
+AlignCost get_const_crab_move_cost(const int from, const int to) {
     return std::abs(from - to);
 }
 
-AlignCost get_linear_crab_move_cost(int from, int to) {
+AlignCost get_linear_crab_move_cost(const int from, const int to) {
     const auto linear_cost = get_const_crab_move_cost(from, to);
     return (linear_cost * (linear_cost + 1)) / 2;
+}
+
+AlignCost get_crabs_align_cost(const int to, const CrabsCount& crabs_count) {
+    AlignCost align_cost = 0;
+    for (const auto& align_from : crabs_count) {
+        const auto one_crab_align_cost = get_linear_crab_move_cost(align_from.first, to);
+        align_cost += (one_crab_align_cost * align_from.second);
+    }
+
+    return align_cost;
 }
 
 int main(int argc, char *argv[]) {
@@ -32,16 +42,21 @@ int main(int argc, char *argv[]) {
     }
 
     auto min_align_cost = std::numeric_limits<AlignCost>::max();
-    for (int align_to = min_crab_pos; align_to <= max_crab_pos; ++align_to) {
-        AlignCost align_cost = 0;
-        for (const auto& align_from : crabs_count) {
-            const auto one_crab_align_cost = get_linear_crab_move_cost(align_from.first, align_to);
-            align_cost += (one_crab_align_cost * align_from.second);
-        }
+    int left = min_crab_pos;
+    int right = max_crab_pos;
+    while (left <= right) {
+        const auto align_to = left + (right - left) / 2;
+        const auto align_to_cost = get_crabs_align_cost(align_to, crabs_count);;
+        const auto align_to_next_cost = get_crabs_align_cost(align_to + 1, crabs_count);;
 
-        if (align_cost < min_align_cost) {
-            min_align_cost = align_cost;
+        if (align_to_next_cost > align_to_cost) {
+            min_align_cost = std::min(min_align_cost, align_to_cost);
+            right = align_to - 1;
+        } else if (align_to_next_cost < align_to_cost) {
+            min_align_cost = std::min(min_align_cost, align_to_next_cost);
+            left = align_to + 1;
         } else {
+            min_align_cost = std::min(min_align_cost, align_to_cost);
             break;
         }
     }
