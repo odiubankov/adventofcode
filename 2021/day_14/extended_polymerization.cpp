e568a5ef33b6615e902f7124d4acdbc274ac8cc0
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2021-12-14 10:20:28 +0100

    Day 14 solution part 2

--- 2021/day_14/extended_polymerization.cpp
+++ 2021/day_14/extended_polymerization.cpp
@@ -1,15 +1,12 @@
 #include <string>
 #include <fstream>
 #include <unordered_map>
-#include <sstream>
 #include <iostream>
 
 namespace {
-constexpr std::size_t part_one_iterations = 10;
-constexpr std::size_t part_two_iterations = 40;
-
 using InsertionRules = std::unordered_map<std::string, char>;
-using CharacterCount = std::unordered_map<char, std::size_t>;
+using CharacterCount = std::unordered_map<char, unsigned long long>;
+using PairsCount = std::unordered_map<std::string, unsigned long long>;
 
 std::string read_polymer_template(std::ifstream& input_stream) {
     std::string input_string;
@@ -31,41 +28,52 @@ InsertionRules read_insertion_rules(std::ifstream& input_stream) {
     return insertion_rules;
 }
 
-std::string apply_insertion_rules(
-        const std::string& polymer_template,
-        const InsertionRules& insertion_rules,
-        std::size_t count) {
-    auto next_template = polymer_template;
-    for (std::size_t i = 0; i < count; ++i) {
-        std::stringstream iteration_template;
-        iteration_template << next_template.front();
-        for (std::size_t character_position = 1; character_position < next_template.size(); ++character_position) {
-            const auto prev_character_position = character_position - 1;
-            const auto rule_key = next_template.substr(prev_character_position, 2);
-            iteration_template << insertion_rules.at(rule_key);
-            iteration_template << next_template.at(character_position);
-        }
-
-        next_template = iteration_template.str();
+PairsCount count_pairs(const std::string& polymer) {
+    PairsCount pairs_count;
+    for (std::size_t i = 1; i < polymer.size(); ++i) {
+        const auto key = polymer.substr(i - 1, 2);
+        ++pairs_count[key];
     }
 
-    return next_template;
+    return pairs_count;
 }
 
-std::size_t get_min_max_difference(const std::string& polymer) {
-    CharacterCount character_count;
-    for (const auto character : polymer) {
-        ++character_count[character];
+unsigned long long grow_polymer_and_get_min_max( 
+        const std::string& polymer,
+        std::size_t steps,
+        const InsertionRules& insertion_rules) {
+    auto pairs_count = count_pairs(polymer);
+    for (std::size_t i = 0; i < steps; ++i) {
+        PairsCount step_pairs_count;
+        for (const auto& pair : pairs_count) {
+            std::string new_first_pair{pair.first.front()};
+            new_first_pair.push_back(insertion_rules.at(pair.first));
+            step_pairs_count[new_first_pair] += pair.second;
+
+            std::string new_second_pair{new_first_pair.back()};
+            new_second_pair.push_back(pair.first.back());
+            step_pairs_count[new_second_pair] += pair.second;
+        }
+        pairs_count = std::move(step_pairs_count);
     }
 
-    std::size_t min_count = std::numeric_limits<std::size_t>::max();
-    std::size_t max_count = 0;
+    CharacterCount character_count;
+    for (const auto& pair_count : pairs_count) {
+        character_count[pair_count.first.front()] += pair_count.second;
+        character_count[pair_count.first.back()] += pair_count.second;
+    }
+
+    ++character_count[polymer.front()];
+    ++character_count[polymer.back()];
+
+    unsigned long long min_count = std::numeric_limits<unsigned long long>::max();
+    unsigned long long max_count = 0;
     for (const auto& count : character_count) {
         min_count = std::min(min_count, count.second);
         max_count = std::max(max_count, count.second);
     }
 
-    return max_count - min_count;
+    return (max_count - min_count) / 2;
 }
 
 }
@@ -75,8 +83,9 @@ int main(int argc, char *argv[]) {
     std::ifstream input_stream{input_path};
     const auto polymer_template = read_polymer_template(input_stream);
     const auto insertion_rules = read_insertion_rules(input_stream);
+    auto pairs_count = count_pairs(polymer_template);
 
-    const auto adapted_polymer = apply_insertion_rules(polymer_template, insertion_rules, part_one_iterations);
-    std::cout << "part 1 answer " << get_min_max_difference(adapted_polymer) << std::endl;
+    std::cout << "after 10 " << grow_polymer_and_get_min_max(polymer_template, 10, insertion_rules) << std::endl;
+    std::cout << "after 40 " << grow_polymer_and_get_min_max(polymer_template, 40, insertion_rules) << std::endl;
     return 0;
 }
